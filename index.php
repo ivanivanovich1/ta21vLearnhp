c5d369a1960dd398456e8e2a1cfe4f46a3bb1007
Pavel Bogatõr <pavel.bogator@tptlive.ee>
2023-09-20 09:50:10 +0300

    Classes

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
 <?php
 
-$a = 5;
+class Box
+{
+    protected $width;
+    protected $height;
+    protected $length;
 
-echo $a;
+    public function _construct($_width, $_height, $_length)
+    {
+        $this->width = $_width;
+        $this->height = $_height;
+        $this->length = $_length;
+    }
+
+    public function volume()
+    {
+        return $this->width * $this->height * $this->length;
+    }
+}
+
+trait Colorful
+{
+    public $color;
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+}
+
+class MetalBox extends Box
+{
+    use Colorful;
+
+    public $weight;
+
+    public function mass()
+    {
+        return $this->volume() * $this->weight;
+    }
+}
+
+$metalBox = new MetalBox(3, 2, 1);
+
+$metalBox->color = "white";
+
+echo $metalBox->getColor();
